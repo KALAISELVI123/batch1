e2fadeb4bf43f7e86dafca66f67ea32d77780e89
Priyadharshnib <priyab4ucool@gmail.com>
2020-01-10 12:54:23 +0530

    modified by priyab

--- src/main/java/myApplication/BasePage.java
+++ src/main/java/myApplication/BasePage.java
@@ -10,12 +10,12 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-public class BasePage {
+public class BasePage1 {
 //edited
 	public static WebDriver driver;
 	public String browser = "chrome";
 
-	public BasePage() {
+	public BasePage1() {
 		if (driver == null) {
 			if (browser.equals("chrome")) {
 				System.setProperty("webdriver.chrome.driver", "D:\\ChromeDriver\\chromedriver.exe");
