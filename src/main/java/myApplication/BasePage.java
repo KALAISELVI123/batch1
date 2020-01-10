fd7fe00140d935ba20eeb6e49a78297151190636
Sujithak95 <sujisai95@gmail.com>
2020-01-10 13:11:09 +0530

    priya

--- src/main/java/myApplication/BasePage.java
+++ src/main/java/myApplication/BasePage.java
@@ -11,7 +11,7 @@ import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class BasePage1 {
-//edited
+//edited by priya
 	public static WebDriver driver;
 	public String browser = "chrome";
 
